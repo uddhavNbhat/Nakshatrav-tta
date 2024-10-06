7f08046e49c507d44ec3b544152cbb307c884c4f
OkayStark <66514398+OkayStark@users.noreply.github.com>
2024-10-06 12:05:05 +0530

    Home page enhancements

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -10,19 +10,26 @@ const Home = () => {
                 <source src="planetVid.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
             <div className="header">
                 <div className="text-wrapper">
-                    <h1 className="main-heading">Explore the Cosmos: A Journey Through Space</h1>
-                </div>
-                <div className="text-wrapper">
-                    <p className="sub-heading">An interactive exploration of celestial bodies, including planets, asteroids, and comets</p>
+                    <h1 className="main-heading">Explore the Cosmos: An interactive exploration of celestial bodies</h1>
                 </div>
             </div>
             <div className="orrery">
-                <Link className="btn btn-primary btn-lg" to="/solar" role="button">Launch Orrery</Link>
+                <Link className="btn btn-primary btn-lg custom-button" to="/solar" role="button">
+                    Launch Orrery 🚀
+                </Link>
             </div>
         </div>
     );
-}
+};
 
 export default Home;
