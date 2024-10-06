5096e6810b7a492d8d44a80e36463674bcaa1d97
AkshayMD18 <amdavanageri@gmail.com>
2024-10-06 11:25:38 +0530

    frontpage

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -6,12 +6,16 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <div className="main-container">
+            <video autoPlay muted loop className="background-video">
+                <source src="planetVid.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             <div className="header">
                 <div className="text-wrapper">
-                    <h1 className="display-4">Explore the Cosmos: A Journey Through Space</h1>
+                    <h1 className="main-heading">Explore the Cosmos: A Journey Through Space</h1>
                 </div>
                 <div className="text-wrapper">
-                    <p className="lead">An interactive exploration of celestial bodies, including planets, asteroids, and comets</p>
+                    <p className="sub-heading">An interactive exploration of celestial bodies, including planets, asteroids, and comets</p>
                 </div>
             </div>
             <div className="orrery">
